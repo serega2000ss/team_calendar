9b39ba3fe4af7c2b9f5fa8c598949609cef51b52
Sergey Korolyuk <serega2000ss@gmail.com>
2014-09-27 12:48:53 +0300

    layout and admin seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+admin = User.create!(
+    :full_name => 'Admin',
+    :email => 'admin@mail.com',
+    :password => '1q2w3e4r',
+    :password_confirmation => '1q2w3e4r',
+    :admin => true
+)
