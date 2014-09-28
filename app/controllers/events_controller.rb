a218b7e271a66f941ebb7f1ce75bbb4f7916602e
Sergey Korolyuk <serega2000ss@gmail.com>
2014-09-28 16:00:01 +0300

    create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -9,4 +9,16 @@ class EventsController < ApplicationController
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(params.require(:event).permit(:name, :event_type_id, :due_date, :description))
+    if @event.save
+      @event.assign_users_by_ids(params.require(:users).keys) if params[:users]
+      flash[:notice] = 'Event created'
+      redirect_to root_path
+    else
+      flash[:danger] = "Can't create Event due to some errors!"
+      render action: :new
+    end
+  end
+
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -16,4 +16,19 @@ class Event < ActiveRecord::Base
 
   has_many :event_users
   has_many :users, through: :event_users
+
+
+  def assign_users_by_ids(user_ids)
+    user_ids.each do |id|
+      user = User.find_by_id(id)
+      self.assign_user(user) if user
+    end
+  end
+
+  def assign_user(user)
+    if EventUser.where(event_id: self.id, user_id: user.id).empty?
+      EventUser.create!(event_id: self.id, user_id: user.id)
+    end
+  end
+
 end
